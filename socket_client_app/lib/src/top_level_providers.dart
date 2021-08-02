ddf7225f122bd269ba6ced429523a2f7c5d0c3cf
garysm <gary@sabiasoftware.com>
2021-08-02 16:38:22 -0400

    add client app

--- socket_client_app/lib/src/top_level_providers.dart
+++ socket_client_app/lib/src/top_level_providers.dart
@@ -1,1 +1,24 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
+/// Creates a WebSocketChannel connected to the server
+final _channelProvider = Provider.autoDispose<IOWebSocketChannel>((ref) {
+  final channel = IOWebSocketChannel.connect(
+    Uri.parse('ws://localhost:8080'),
+  );
+  ref.onDispose(() => channel.sink.close(status.goingAway));
+  return channel;
+});
+
+/// Returns the stream of data sent from the channel
+final channelStreamProvider = StreamProvider.autoDispose(
+  (ref) => ref.watch(_channelProvider).stream,
+);
+
+/// Send data to the channel sink
+final addToSinkProvider =
+    Provider.autoDispose.family<void, dynamic>((ref, data) {
+  final channel = ref.watch(_channelProvider);
+  return channel.sink.add(data);
+});
